428d5eec341070dae900f045a3db443892088955
monkstone <mamba2928@yahoo.co.uk>
2017-07-04 14:54:14 +0100

    refactor

--- test/has_constants_test.rb
+++ test/has_constants_test.rb
@@ -3,23 +3,26 @@ require 'java'
 require_relative 'test_helper'
 require_relative '../lib/pconstants'
 
-java_import Java::ProcessingCore::PConstants
+module Constants
+  java_import 'processing.core.PConstants'
+  PConstants
+end
 
 class SpecTest < Minitest::Test
-  include PConstants
+  include Constants
 
-  def test_pconstants_is_a_module
-    assert_equal Module, PConstants.class, 'PConstants not recognized as a Module'
+  def test_constants_is_a_module
+    assert_equal Module, Constants.class, 'Constants not recognized as a module'
   end
 
   def test_p3d
-    assert_equal String, P3D.class, "failed #{:P3D} is a string"
-    assert_equal Java::JavaLang::String, P3D.to_java(:string).class, "failed #{:P3D} can be cast as a java string"
-    assert_equal 'processing.opengl.PGraphics3D', P3D, "failed #{:P3D} lookup"
+    assert_equal String, PConstants::P3D.class, "failed #{:P3D} is a string"
+    assert_equal Java::JavaLang::String, PConstants::P3D.to_java(:string).class, "failed #{:P3D} can be cast as a java string"
+    assert_equal 'processing.opengl.PGraphics3D', PConstants::P3D, "failed #{:P3D} lookup"
   end
 
   def test_numeric
-    assert_equal 8, TRIANGLE, "failed #{:TRIANGLE} lookup"
-    assert_in_delta 3.14159, PI, 0.001, "failed #{:PI} lookup"
+    assert_equal 8, PConstants::TRIANGLE, "failed #{:TRIANGLE} lookup"
+    assert_in_delta 3.14159, PConstants::PI, 0.0001, "failed #{:PI} lookup"
   end
 end
